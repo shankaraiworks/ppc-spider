224c4050ab538c6b494a6712d5a22c7984df27af
Shankar AI <shankar.ai.works@gmail.com>
2025-09-07 15:27:56 -0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,7 @@
-
-
+import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './src/app.component';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
